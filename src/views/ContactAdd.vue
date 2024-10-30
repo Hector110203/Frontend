1ce62452f39c87197d1e9f63a5f9b9107ee8bb2f
Hector110203 <hautrung110203@gmail.com>
2024-10-31 01:39:03 +0700

    Them trang tạo lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h4>Thêm liên hệ mới</h4>
+    <!-- Form liên hệ hoặc các phần tử giao diện để thêm liên hệ -->
+    <ContactForm 
+    :contact="contact"
+    @submit:contact="createContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: {
+                name: "",
+                email: "",
+                address: "",
+                phone: "",
+                favorite: false,
+            },
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = "Liên hệ đã được thêm thành công.";
+                this.contact = {
+                    name: "",
+                    email: "",
+                    address: "",
+                    phone: "",
+                    favorite: false,
+                };
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -13,6 +13,8 @@
         :contacts="filteredContacts"
         v-model:activeIndex="activeIndex"
       />
+      
+      
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
